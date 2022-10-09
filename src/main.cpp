8d354c0101804e78c1cf7f466740d2f5741f581d
BaukeWestendorp <baukewestendorp@gmail.com>
2022-10-09 14:40:39 +0200

    feat: Add switch

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,19 @@
 #include <LiquidCrystal.h>
 #include <countries.h>
 
-const int rs = 2, en = 3, d4 = 7, d5 = 8, d6 = 9, d7 = 10;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+#define PIN_SWITCH 5
+#define PIN_RS 2
+#define PIN_EN 3
+#define PIN_D4 7
+#define PIN_D5 8
+#define PIN_D6 9
+#define PIN_D7 10
 
-void setup() {
-	lcd.begin(16, 2);
-	lcd.clear();
-	lcd.print("Spin the Wheel!");
-}
+LiquidCrystal lcd(PIN_RS, PIN_EN, PIN_D4, PIN_D5, PIN_D6, PIN_D7);
 
-void loop() {
-	delay(1000);
+bool p_pin_state = LOW;
+
+void tick() {
 	lcd.clear();
 
 	const char *country = pick_country();
@@ -19,4 +21,21 @@ void loop() {
 		if (country[i] == '#') lcd.setCursor(0, 1);
 		else lcd.print(country[i]);
 	}
+}
+
+void setup() {
+	lcd.begin(16, 2);
+	lcd.clear();
+	lcd.print("Spin the Wheel!");
+
+	pinMode(PIN_SWITCH, INPUT_PULLUP);
+}
+
+void loop() {
+	bool pin_state = digitalRead(PIN_SWITCH);
+	if (p_pin_state == HIGH && pin_state == LOW)
+		tick();
+	p_pin_state = pin_state;
+
+	delay(20);
 }
